1202f6a63a8521d15991591d02a6be56b1e5dd4b
Tomáš Blatný <blatny.tomas@seznam.cz>
2016-09-08 13:32:20 +0200

    Task 5: tasks can be filtered by name now

--- app/model/repository/AbstractRepository.php
+++ app/model/repository/AbstractRepository.php
@@ -1,31 +1,48 @@
 <?php
 namespace App\Model\Repository;
 
-use Nette\Object;
 use Kdyby\Doctrine\EntityManager;
+use Nette\Object;
+
 
 abstract class AbstractRepository extends Object
 {
-    /** @var EntityManager */
-    protected $entityManager;
 
-    public function __construct(EntityManager $entityManager)
-    {
-        $this->entityManager = $entityManager;
-    }
+	/** @var EntityManager */
+	protected $entityManager;
 
-    public function updateEntity($entity)
-    {
-        $this->entityManager->persist($entity);
-        $this->entityManager->flush();
-    }
 
-    public function delete($id)
-    {
-        $entity = $this->getById($id);
-        $this->entityManager->remove($entity);
-        $this->entityManager->flush();
-    }
+	public function __construct(EntityManager $entityManager)
+	{
+		$this->entityManager = $entityManager;
+	}
 
-    abstract public function getById($id);
+
+	public function updateEntity($entity)
+	{
+		$this->entityManager->persist($entity);
+		$this->entityManager->flush();
+	}
+
+
+	public function delete($id)
+	{
+		$entity = $this->getById($id);
+		$this->entityManager->remove($entity);
+		$this->entityManager->flush();
+	}
+
+
+	abstract public function getById($id);
+
+
+	/**
+	 * Escapes argument used in LIKE statement to prevent wildcard injection
+	 * @param string $arg
+	 * @return string
+	 */
+	protected function escapeLikeArg($arg)
+	{
+		return addcslashes($arg, '%_');
+	}
 }
